6d32e94a1f1c5fe7fe622e67526627b2f0bcaca7
Takashi Sato <takashisato.develop@gmail.com>
2023-07-09 18:22:30 +0900

    feat: Articleモデル追加

---
+++ apps/api/src/prisma/migrations/20230709082002_add_article/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
